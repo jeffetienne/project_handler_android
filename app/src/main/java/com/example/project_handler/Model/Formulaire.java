58bfb99ea979d09c73f3ea42a12186e14c7cda5c
Jeff Etienne <jeetic9@gmail.com>
2019-12-31 16:14:00 -0500

    Add Formulaire Question and Reponse

---
+++ app/src/main/java/com/example/project_handler/Model/Formulaire.java
@@ -0,0 +1,103 @@
+package com.example.project_handler.Model;
+
+import java.util.Date;
+
+public class Formulaire {
+    private int Id;
+    private String Name;
+    private String Description;
+    private TypeForm TypeForm;
+    private int TypeFormId;
+    private Projet Projet;
+    private int ProjetId;
+    private String CreePar;
+    private Date CreeLe;
+
+    public Formulaire(int id,
+                      String name,
+                      String description,
+                      TypeForm typeForm,
+                      int typeFormId,
+                      Projet projet,
+                      int projetId,
+                      String creePar,
+                      Date creeLe) {
+        Id = id;
+        Name = name;
+        Description = description;
+        TypeForm = typeForm;
+        TypeFormId = typeFormId;
+        Projet = projet;
+        ProjetId = projetId;
+        CreePar = creePar;
+        CreeLe = creeLe;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public com.example.project_handler.Model.TypeForm getTypeForm() {
+        return TypeForm;
+    }
+
+    public void setTypeForm(com.example.project_handler.Model.TypeForm typeForm) {
+        TypeForm = typeForm;
+    }
+
+    public int getTypeFormId() {
+        return TypeFormId;
+    }
+
+    public void setTypeFormId(int typeFormId) {
+        TypeFormId = typeFormId;
+    }
+
+    public com.example.project_handler.Model.Projet getProjet() {
+        return Projet;
+    }
+
+    public void setProjet(com.example.project_handler.Model.Projet projet) {
+        Projet = projet;
+    }
+
+    public int getProjetId() {
+        return ProjetId;
+    }
+
+    public void setProjetId(int projetId) {
+        ProjetId = projetId;
+    }
+
+    public String getCreePar() {
+        return CreePar;
+    }
+
+    public void setCreePar(String creePar) {
+        CreePar = creePar;
+    }
+
+    public Date getCreeLe() {
+        return CreeLe;
+    }
+
+    public void setCreeLe(Date creeLe) {
+        CreeLe = creeLe;
+    }
+}
---
+++ app/src/main/java/com/example/project_handler/Model/Question.java
@@ -0,0 +1,50 @@
+package com.example.project_handler.Model;
+
+import java.util.Date;
+
+public class Question {
+    private int Id;
+    private String Name;
+    private String Message;
+    private Formulaire Formulaire;
+    private int FormulaireId;
+    private Component Component;
+    private int ComponentId;
+    private TypeDonnee TypeDonnee;
+    private int TypeDonneeId;
+    private int Minimum;
+    private int Maximum;
+    private Boolean Required;
+    private String CreePar;
+    private Date CreeLe;
+
+    public Question(int id,
+                    String name,
+                    String message,
+                    Formulaire formulaire,
+                    int formulaireId,
+                    Component component,
+                    int componentId,
+                    TypeDonnee typeDonnee,
+                    int typeDonneeId,
+                    int minimum,
+                    int maximum,
+                    Boolean required,
+                    String creePar,
+                    Date creeLe) {
+        Id = id;
+        Name = name;
+        Message = message;
+        Formulaire = formulaire;
+        FormulaireId = formulaireId;
+        Component = component;
+        ComponentId = componentId;
+        TypeDonnee = typeDonnee;
+        TypeDonneeId = typeDonneeId;
+        Minimum = minimum;
+        Maximum = maximum;
+        Required = required;
+        CreePar = creePar;
+        CreeLe = creeLe;
+    }
+}
