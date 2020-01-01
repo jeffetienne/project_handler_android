b9bf07eafce5667ed61b59fa6b96c73b8acb9ef1
jeffetienne <jeetic9@gmail.com>
2020-01-01 01:34:08 -0500

    Adapter

---
+++ app/src/main/java/com/example/project_handler/Data/ProjetRecyclerViewAdapter.java
@@ -0,0 +1,72 @@
+package com.example.project_handler.Data;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.project_handler.Model.Projet;
+import com.example.project_handler.R;
+
+import java.util.List;
+
+public class ProjetRecyclerViewAdapter extends RecyclerView.Adapter<ProjetRecyclerViewAdapter.ViewHolder> {
+    private Context context;
+    private List<Projet> projets;
+
+    public ProjetRecyclerViewAdapter(Context context, List<Projet> projets) {
+        this.context = context;
+        this.projets = projets;
+    }
+
+    @Override
+    public ProjetRecyclerViewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.projet_activity, parent, false);
+        return new ViewHolder(view, context);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Projet projet = this.projets.get(position);
+
+        holder.Name.setText(projet.getName());
+        holder.Description.setText(projet.getDescription());
+        holder.Domaine.setText(projet.getDomaine().getName());
+        holder.CreePar.setText(projet.getCreePar());
+        holder.CreeLe.setText(projet.getCreeLe().toString());
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.projets.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView Name;
+        TextView Description;
+        TextView Domaine;
+        TextView CreePar;
+        TextView CreeLe;
+
+        public ViewHolder(View itemView, Context ctx){
+            super(itemView);
+            context = ctx;
+
+            Name = (TextView) itemView.findViewById(R.id.projetName);
+            Description = (TextView) itemView.findViewById(R.id.projetDescription);
+            Domaine = (TextView) itemView.findViewById(R.id.projetDomaine);
+            CreePar = (TextView) itemView.findViewById(R.id.projetCreePar);
+            CreeLe = (TextView) itemView.findViewById(R.id.projetCreeLe);
+        }
+
+
+        public void onClick(View v){
+
+        }
+    }
+}
